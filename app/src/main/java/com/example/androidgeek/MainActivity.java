134502ee0265be403231a29bab907814f258c0e9
Vladimir Danilov <danilov17792@yandex.ru>
2020-05-21 02:49:28 +0300

    Задание ко второму уроку готово

--- app/src/main/java/com/example/androidgeek/MainActivity.java
+++ app/src/main/java/com/example/androidgeek/MainActivity.java
@@ -3,12 +3,72 @@ package com.example.androidgeek;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView lv;
+    ArrayAdapter<String> adapter;
+    EditText inputSearch;
+    ArrayList<HashMap<String, String>> words;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_city_selection);
+
+        String words[] = {"Москва", "Санкт-Петербург", "Самара", "Нижний Новгород", "Владимир",
+                "Тольятти", "Казань", "Ставрополь", "Екатеринбург", "Тюмень"};
+
+        lv = (ListView) findViewById(R.id.list_view_city_search);
+        inputSearch = (EditText) findViewById(R.id.inputSearch);
+
+        adapter = new ArrayAdapter<String>(this, R.layout.city_list,
+                R.id.product_name, words);
+        lv.setAdapter(adapter);
+
+        inputSearch.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence cs, int arg1, int arg2, int arg3) {
+                MainActivity.this.adapter.getFilter().filter(cs);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+                                          int arg3) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable arg0) {
+            }
+        });
+
+        inputSearch.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence cs, int arg1, int arg2, int arg3) {
+                MainActivity.this.adapter.getFilter().filter(cs);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+                                          int arg3) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable arg0) {
+            }
+        });
+
     }
+
 }
+
